34bbb794f43564808ad57fe99e9b65d355e8cee3
vanillovin <vanillovin@gmail.com>
2022-12-20 13:47:40 +0900

    카운터 숫자 구현하기

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,10 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
+  // App 컴포넌트를 렌더링
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // screen object를 이용해 원하는 엘리먼트에 접근 (ID로 접근)
+  const counterElement = screen.getByTestId('counter');
+  // id가 counter인 엘리먼트의 텍스트가 0인지 테스트
+  expect(counterElement).toHaveTextContent(0);
 });
